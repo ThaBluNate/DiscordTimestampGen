ff1bd7cef7b6e5b227fb2e4ca38af6e4b65fd526
ThaBluNate <51035517+ThaBluNate@users.noreply.github.com>
2023-11-05 04:02:23 -0500

    Add files via upload

---
+++ window.js
@@ -0,0 +1,30 @@
+function c(e){return document.getElementById(e)}
+function a(e){return c(e).value}
+function b(e){return c(e)[c(e).selectedIndex].value}
+d=new Date()
+
+onload=function(){reset()}
+function reset(){
+    c('event').value="Event"
+    c('at').value="@"
+    c('year').value=d.getFullYear()
+    c('month').selectedIndex=d.getMonth()
+    c('day').value=d.getDate()
+    c('hour').value=d.getHours()
+    c('minute').value=d.getMinutes()
+    c('minute').value=a('minute').padStart(2,0)
+    c('time').value=Math.abs(new Date(0).getTimezoneOffset())/60
+    Math.sign(d.getTimezoneOffset()) == -1 ? c('tid').selectedIndex = 0 : c('tid').selectedIndex = 1
+    c('out').value=""
+    c('incTime').selectedIndex=0
+    c('incRel').selectedIndex=0
+}
+
+function getTS(){return Math.floor(Date.parse(`${a('day')} ${b('month')} ${a('year')} ${a('hour')}:${a('minute')}:00 UTC${b('tid')}${a('time')}`)/1000)}
+function getMSG(){
+    if(b('incRel') == 1){
+        return `${a('event')} ${a('at')} <t:${getTS()}:${b('incTime')}>, <t:${getTS()}:R>`
+    } else {
+        return `${a('event')} ${a('at')} <t:${getTS()}:${b('incTime')}>`
+    }
+}
