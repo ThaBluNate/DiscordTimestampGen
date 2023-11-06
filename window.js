b477e476da1adc6e91ce4a7a65dc79383ca7742c
ThaBluNate <51035517+ThaBluNate@users.noreply.github.com>
2023-11-05 20:06:45 -0500

    extend format options

--- window.js
+++ window.js
@@ -1,10 +1,10 @@
 function c(e){return document.getElementById(e)}
 function a(e){return c(e).value}
 function b(e){return c(e)[c(e).selectedIndex].value}
-d=new Date()
 
 onload=function(){reset()}
 function reset(){
+    d=new Date()
     c('event').value="Event"
     c('at').value="@"
     c('year').value=d.getFullYear()
@@ -16,15 +16,15 @@ function reset(){
     c('time').value=Math.abs(new Date(0).getTimezoneOffset())/60
     Math.sign(d.getTimezoneOffset()) == -1 ? c('tid').selectedIndex = 0 : c('tid').selectedIndex = 1
     c('out').value=""
-    c('incTime').selectedIndex=0
+    c('fmt').selectedIndex=0
     c('mode').selectedIndex=0
 }
 
 function getTS(){return Math.floor(Date.parse(`${a('day')} ${b('month')} ${a('year')} ${a('hour')}:${a('minute')}:00 UTC${b('tid')}${a('time')}`)/1000)}
 function getMSG(){
     switch(b('mode')){
-        case '0': return `${a('event')} ${a('at')} <t:${getTS()}:${b('incTime')}>, <t:${getTS()}:R>`
-        case '1': return `${a('event')} ${a('at')} <t:${getTS()}:${b('incTime')}>`
+        case '0': return `${a('event')} ${a('at')} <t:${getTS()}:${b('fmt')}>, <t:${getTS()}:R>`
+        case '1': return `${a('event')} ${a('at')} <t:${getTS()}:${b('fmt')}>`
         case '2': return `${a('event')} ${a('at')} <t:${getTS()}:R>`
     }
 }
